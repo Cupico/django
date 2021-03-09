2802c32de31f1e1ab7ea0bd57ed20a69954f6c88
David <davidelbar2@gmail.com>
2021-03-09 11:38:52 +0100

    ok

--- djangosite/views.py
+++ djangosite/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    title = "On est sur la homepage"
     #return HttpResponse('base.html')
-    return render(request, 'main.html', {"title": title})
+    return render(request, 'index.html')
 
 
 def about(request):
