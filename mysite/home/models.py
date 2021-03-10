3557918eb9f5515fef4a0710095b2af3ed289c69
David <davidelbar2@gmail.com>
2021-03-10 11:09:12 +0100

    ok

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+
